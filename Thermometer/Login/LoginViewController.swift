b6d923eafbba4041692c252ba8788aa4d9a71c2c
saturn777 <aleksandr-luchinin@bk.ru>
2022-10-27 20:14:55 -0700

    Создаю Алертконтроллер , который работают при входе на проверку логина и пароля

--- Thermometer/Login/LoginViewController.swift
+++ Thermometer/Login/LoginViewController.swift
@@ -1,66 +1,76 @@
-//
-//  LoginViewController.swift
-//  Thermometer
+
 //
 //  Created by Aleksandr Luchinin on 14.10.2022.
-//
+///
 
 import UIKit
 
-class LoginViewController: UIViewController {
-
-  
-        @IBOutlet weak var loginTextField: UITextField!
-        @IBOutlet weak var passwordTextField: UITextField!
-        @IBOutlet weak var loginButton: UIButton!
-        @IBOutlet weak var scrollView: UIScrollView!
+final class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShow(_:)), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(onTap))
+        recognizer.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(recognizer)
+    }
+    
+    
+    @objc func keyboardDidShow(_ notification: Notification) {
+        guard let keyboardHeight = ((notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue)?.height else {return}
+        let insets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardHeight, right: 0)
+        scrollView.contentInset = insets
+        scrollView.scrollIndicatorInsets = insets
+    }
+    
+    
+    @objc func onTap() {
+        self.view.endEditing(true)
+    }
+    
+    @objc func keyboardDidHide() {
+        scrollView.contentInset = UIEdgeInsets.zero
+        scrollView.scrollIndicatorInsets = UIEdgeInsets.zero
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        guard let login = loginTextField.text,
+              let password = passwordTextField.text else {return}
         
         
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShow(_:)), name: UIResponder.keyboardDidShowNotification, object: nil)
-            NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        override func shouldPerformSegue(withIdentifier identifier: String, sender:
+                                            Any?) -> Bool {
             
-            let recognizer = UITapGestureRecognizer(target: self, action: #selector(onTap))
-            recognizer.cancelsTouchesInView = false
-            self.view.addGestureRecognizer(recognizer)
-        }
-        
-        
-        @objc func keyboardDidShow(_ notification: Notification) {
-            guard let keyboardHeight = ((notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue)?.height else {return}
-            let insets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardHeight, right: 0)
-            scrollView.contentInset = insets
-            scrollView.scrollIndicatorInsets = insets
-        }
-        
-        
-        @objc func onTap() {
-            self.view.endEditing(true)
-        }
-        
-        @objc func keyboardDidHide() {
-            scrollView.contentInset = UIEdgeInsets.zero
-            scrollView.scrollIndicatorInsets = UIEdgeInsets.zero
-        }
-        
-        deinit {
-            NotificationCenter.default.removeObserver(self)
-        }
-        
-        @IBAction func loginButtonPressed(_ sender: Any) {
-            guard let login = loginTextField.text,
-                  let password = passwordTextField.text else {return}
+            let login = loginInput.text!
+            let password = passwordInput.text!
             
-            if login == "root",
-               password == "1234" {
-                print("Success")
-            }
-            else {
-                print("login error")
-            }
+            if login == "admin" && password == "1234" {
+                return true
+            } else {
+                // Создаю Алерт контроллер
+                let alert = UIAlertController(title: "Ошибка", message: "Введены неверные данные пользователя", preferredStyle: .alert)
+                // Создаю кнопку для Алерконтролера
+                let action = UIAlertAction(title: "OK", style: .cancel, handler:
+                                            nil)
+                // Добавляем кнопку на UIAlertController
+                alert.addAction(action)
+                // Показ Алертконтролера
+                present(alert, animated: true, completion: nil)
+                return false        }
             
         }
         
     }
-
+}
